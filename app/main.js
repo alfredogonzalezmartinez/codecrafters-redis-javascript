cf34bc6605122b33cb3516a3cfc9ba4d8d35d855
Alfredo González Martínez <48945157+alfredogonzalezmartinez@users.noreply.github.com>
2023-11-09 15:59:41 +0100

    Implement the ECHO command

--- app/main.js
+++ app/main.js
@@ -7,7 +7,8 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
   // Handle connection
   connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
+    const request = parseRequestData(data);
+    handleRequest(request, connection);
   });
 
   connection.on("close", () => {
@@ -16,3 +17,169 @@ const server = net.createServer((connection) => {
 });
 
 server.listen(6379, "127.0.0.1");
+
+const PROTOCOLS_TERMINATOR = "\r\n";
+
+const CATEGORY = Object.freeze({
+  SIMPLE: "Simple",
+  AGGREGATE: "Aggregate",
+});
+
+const RESP_DATA_TYPE = Object.freeze({
+  SIMPLE_STRING: Object.freeze({
+    FIST_BYTE: "+",
+    CATEGORY: CATEGORY.SIMPLE,
+  }),
+  SIMPLE_ERROR: Object.freeze({
+    FIST_BYTE: "-",
+    CATEGORY: CATEGORY.SIMPLE,
+  }),
+  INTEGER: Object.freeze({
+    FIST_BYTE: ":",
+    CATEGORY: CATEGORY.SIMPLE,
+  }),
+  BULK_STRING: Object.freeze({
+    FIST_BYTE: "$",
+    CATEGORY: CATEGORY.AGGREGATE,
+  }),
+  ARRAY: Object.freeze({
+    FIST_BYTE: "*",
+    CATEGORY: CATEGORY.AGGREGATE,
+  }),
+});
+
+/**
+ * @typedef {Object} RedisRequest
+ * @property {string} command
+ * @property {string[]} arguments
+ */
+
+/**
+ * @param {Buffer} data
+ * @returns {RedisRequest}
+ */
+function parseRequestData(data) {
+  const respArray = data.toString();
+  const [command, ...arguments] = parseRespArrayToArray(respArray);
+  return { command: command.toLocaleUpperCase(), arguments };
+}
+
+function isValidResp(string, respDataType) {
+  const startsWithTheRightByte = string.startsWith(respDataType.FIST_BYTE);
+  if (!startsWithTheRightByte) return false;
+
+  const endsWithTheRightProtocolsTerminator =
+    string.endsWith(PROTOCOLS_TERMINATOR);
+  if (!endsWithTheRightProtocolsTerminator) return false;
+
+  return true;
+}
+
+/**
+ * @param {string} respSimpleString
+ * @returns {string}
+ */
+function parseRespSimpleStringToString(respSimpleString) {
+  if (!isValidResp(respSimpleString, RESP_DATA_TYPE.SIMPLE_STRING)) return "";
+  return respSimpleString.slice(1, -2);
+}
+
+/**
+ * @param {string} respBulkString
+ * @returns {string}
+ */
+function parseRespBulkStringToString(respBulkString) {
+  if (!isValidResp(respBulkString, RESP_DATA_TYPE.BULK_STRING)) return "";
+  return respBulkString.split(PROTOCOLS_TERMINATOR)[1];
+}
+
+/**
+ * @param {string} respArray
+ * @returns {Array}
+ */
+function parseRespArrayToArray(respArray) {
+  if (!isValidResp(respArray, RESP_DATA_TYPE.ARRAY)) return [];
+
+  const [, ...arrayElements] = respArray.split(PROTOCOLS_TERMINATOR);
+
+  const array = [];
+  const arrayElementsAmount = arrayElements.length;
+  for (let i = 0; i < arrayElementsAmount; i++) {
+    const element = arrayElements[i];
+
+    if (element.startsWith(RESP_DATA_TYPE.BULK_STRING.FIST_BYTE)) {
+      i++;
+      const nextElement = arrayElements[i];
+      const respBulkString = `${element}${PROTOCOLS_TERMINATOR}${nextElement}${PROTOCOLS_TERMINATOR}`;
+      array.push(parseRespBulkStringToString(respBulkString));
+    }
+  }
+
+  return array;
+}
+
+/**
+ * @param {string} respSimpleString
+ * @returns {string}
+ */
+function parseStringToRespSimpleString(string) {
+  const firstByte = RESP_DATA_TYPE.SIMPLE_STRING.FIST_BYTE;
+  return `${firstByte}${string}${PROTOCOLS_TERMINATOR}`;
+}
+
+/**
+ * @param {string} respSimpleString
+ * @returns {string}
+ */
+function parseStringToRespSimpleError(string) {
+  const firstByte = RESP_DATA_TYPE.SIMPLE_ERROR.FIST_BYTE;
+  return `${firstByte}${string}${PROTOCOLS_TERMINATOR}`;
+}
+
+const COMMAND_HANDLER = Object.freeze({
+  PING: pingHandler,
+  ECHO: echoHandler,
+  UNKNOWN: unknownHandler,
+});
+
+/**
+ * @param {RedisRequest} request
+ * @param {net.Socket} connection
+ */
+function handleRequest(request, connection) {
+  const { command } = request;
+  const handler = COMMAND_HANDLER[command];
+  if (!handler) return COMMAND_HANDLER.UNKNOWN(request, connection);
+  handler(request, connection);
+}
+
+/**
+ * @param {RedisRequest} request
+ * @param {net.Socket} connection
+ */
+function pingHandler(request, connection) {
+  const pong = "PONG";
+  connection.write(parseStringToRespSimpleString(pong));
+}
+
+/**
+ * @param {RedisRequest} request
+ * @param {net.Socket} connection
+ */
+function echoHandler(request, connection) {
+  const {
+    arguments: [message = ""],
+  } = request;
+
+  connection.write(parseStringToRespSimpleString(message));
+}
+
+/**
+ * @param {RedisRequest} request
+ * @param {net.Socket} connection
+ */
+function unknownHandler(request, connection) {
+  const { command } = request;
+  const error = `ERROR unknown command '${command}'`;
+  connection.write(parseStringToRespSimpleError(error));
+}
